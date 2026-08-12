0e6cc671a08f5c2cc706c0516c3a38486b2c280b
Luis Barrile <lh.barrile@gmail.com>
2026-08-12 20:12:25 -0300

    Adicionado tela de cadastro

--- js/main.js
+++ js/main.js
@@ -10,3 +10,34 @@ function verSenha() {
         icon.src = "../assets/img/hide.png";
     }
 }
+
+function verSenhaCadastro() {
+    const password = document.getElementById('password-cadastro');
+    const icon = document.getElementById('icon-cadastro');
+
+    if (password.type === 'password') {
+        password.type = 'text';
+        icon.src = "../assets/img/show.png";
+    } else {
+        password.type = 'password';
+        icon.src = "../assets/img/hide.png";
+    }
+}
+
+function verSenhaConfirma() {
+    const password = document.getElementById('password-confirma');
+    const icon = document.getElementById('icon-confirma');
+
+    if (password.type === 'password') {
+        password.type = 'text';
+        icon.src = "../assets/img/show.png";
+    } else {
+        password.type = 'password';
+        icon.src = "../assets/img/hide.png";
+    }
+}
+
+function mostrarTela(id) {
+    document.querySelectorAll('.tela').forEach(t => t.classList.remove('ativa'));
+    document.getElementById(id).classList.add('ativa');
+}
